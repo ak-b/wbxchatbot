d6a20be6c76620a11dd1815d458445503b20a6c9
Akansha Bansal (akbansal) <akbansal@cisco.com>
2020-05-15 12:58:59 +0530

    Obsolete files
    Not in use

--- docker_run.sh
+++
@@ -1,2 +0,0 @@
-set -a; source ~/.env; set +a;
-docker run -d -e BOT_NAME={$BOT_NAME} -e BOT_TOKEN={$BOT_TOKEN} -e BOT_USERNAME={$BOT_USERNAME} -e BOT_PASSWORD={$BOT_PASSWORD} -e ENABLE_PASSWORD={$ENABLE_PASSWORD}  -v $PWD/k8s/users.properties:/opt/wibot/users.properties registry-qa.webex.com/marvin:test7
