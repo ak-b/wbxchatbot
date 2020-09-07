b8df102911e1252ab931eb60f696bfd02f7795ae
Akansha <36146495+ak-b@users.noreply.github.com>
2020-09-07 15:03:10 +0530

    Update dryrun.py

--- wibot/cli/firewall/dryrun.py
+++ wibot/cli/firewall/dryrun.py
@@ -18,17 +18,15 @@ def dryrun():
 		}
 
 	dryrun_req = requests.post(url_dryrun, data=json.dumps(req_body), headers=headers)
-	#print(dryrun_req.status_code)
 	if dryrun_req.status_code == 200:
 		print("Dry Run Results for MMP NS FW Configurations")
 		response = json.loads(dryrun_req.content.decode('utf-8'))
 		iteration_devices = response['ciap-fwaction-output']['native']['device']
-		#print(iteration_devices)
 		for device in iteration_devices:
 			print("***************")
 			print("Device : {}.WEBEX.COM Context : {}".format(device['response']['device'], device['response']['context']))
 			print("Configuration:")
-			[print(config) for config in device['response']['config']]
+			print(config) for config in device['response']['config']]
 			print("***************")
 	else:
 		print("Dry Run Encountered an Error, details down below:\n")
