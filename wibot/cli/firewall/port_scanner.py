c60fe418f06cafad4d9d0f79a960201e07efe85f
Akansha Bansal (akbansal) <akbansal@cisco.com>
2020-12-03 14:18:48 +0530

    Update port_scanner.py

--- wibot/cli/firewall/port_scanner.py
+++ wibot/cli/firewall/port_scanner.py
@@ -1,31 +1,40 @@
 '''
 Script to use nmap to help verify ports are open on certain devices.
 Nmap: Nmap is a free and open-source network scanning tool. https://pypi.org/project/python3-nmap/
+import nmap3
+nmap = nmap3.Nmap()
+results = nmap.scan_top_ports("your-host.com")
+# And you would get your results in json
 '''
-import nmap 
+import nmap3
+
+def scan_port(port_number):
    
-# take the range of ports to  
-# be scanned 
-begin = 75
-end = 80
-  
-# assign the target ip to be scanned to 
-# a variable 
-target = '127.0.0.1'
-   
-# instantiate a PortScanner object 
-scanner = nmap.PortScanner() 
-   
-for i in range(begin,end+1): 
-   
-    # scan the target port 
-    res = scanner.scan(target,str(i)) 
-   
-    # the result is a dictionary containing  
-    # several information we only need to 
-    # check if the port is opened or closed 
-    # so we will access only that information  
-    # in the dictionary 
-    res = res['scan'][target]['tcp'][i]['state'] 
-   
-    print(f'port {i} is {res}.') 
+    # take the range of ports to  
+    # be scanned 
+    begin = 75
+    end = 80
+
+    # assign the target ip to be scanned to 
+    # a variable 
+    target = '127.0.0.1'
+
+    # instantiate a PortScanner object 
+    scanner = nmap3.PortScanner() 
+
+    for i in range(begin,end+1): 
+
+        # scan the target port 
+        res = scanner.scan(target,str(i)) 
+
+        # the result is a dictionary containing  
+        # several information we only need to 
+        # check if the port is opened or closed 
+        # so we will access only that information  
+        # in the dictionary 
+        res = res['scan'][target]['tcp'][i]['state'] 
+
+        print(f'port {i} is {res}.') 
+        
+  if __name__ == "__init__":
+    scan_port('80')
